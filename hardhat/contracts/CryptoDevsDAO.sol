4d415e77d259c94382a1efe52737b2bf78b33a67
Lerato Rampai <87371242+leratomakhasane@users.noreply.github.com>
2022-08-06 14:41:39 +0200

    initialize variables

--- hardhat/contracts/CryptoDevsDAO.sol
+++ hardhat/contracts/CryptoDevsDAO.sol
@@ -3,4 +3,121 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CryptoDevsDAO is Ownable{}
+interface IFakeNFTMarketplace{
+    function getPrice() external view returns (uint256);
+
+    function available(uint256 _tokenId) external view returns(bool);
+
+    function purchase(uint256 _tokenId) external payable;
+}
+
+interface ICryptoDevsNFT{
+    function balanceOf(address owner) external view returns (uint256);
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+}
+
+contract CryptoDevsDAO is Ownable{
+    IFakeNFTMarketplace nftMarketplace;
+    ICryptoDevsNFT cryptoDevsNFT;
+
+    struct Proposal {
+        uint256 nftTokenId;
+        uint256 deadline;
+        uint256 yayVotes;
+        uint256 nayVotes;
+        bool executed;
+
+        mapping(uint256 => bool) voters;
+    }
+
+    //ID to Proposal
+    mapping(uint256 => Proposal) public proposals;
+
+    uint256 public numProposals;
+
+    constructor(address _nftMarketplace, address _cryptoDevsNFT) payable {
+        nftMarketplace = IFakeNFTMarketplace(_nftMarketplace);
+        cryptoDevsNFT = ICryptoDevsNFT(_cryptoDevsNFT);
+    }
+
+    modifier nftHolderOnly() {
+        require(cryptoDevsNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+        _;
+    }
+
+    function createProposal(uint256 _nftTokenId) external nftHolderOnly returns(uint256) {
+        require(nftMarketplace.available(_nftTokenId), "NFT_NOT_FOR_SALE");
+        Proposal storage proposal = proposals[numProposals];
+        proposal.nftTokenId = _nftTokenId;
+
+        proposal.deadline = block.timestamp + 5 minutes;
+        numProposals++;
+
+        return numProposals - 1;
+    }
+
+    modifier activeProposalOnly(uint256 proposalIndex) {
+        require(proposals[proposalIndex].deadline > block.timestamp, "DEADLINE_EXCEEDED");
+        _;
+    }
+
+    enum Vote {
+        //yay = 0, nay = 1
+        YAY,
+        NAY
+    }
+
+    function voteOnProposal(uint256 proposalIndex, Vote vote) external nftHolderOnly activeProposalOnly(proposalIndex) {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        uint256 voterNFTBalance = cryptoDevsNFT.balanceOf(msg.sender);
+        uint256 numVotes = 0;
+
+        //calculate number of NFTs owned by voter, that have not being used for voting
+        for(uint256 i = 0; i < voterNFTBalance; i++) {
+            uint256 tokenId = cryptoDevsNFT.tokenOfOwnerByIndex(msg.sender, i);
+
+            if(proposal.voters[tokenId] == false) {
+                numVotes++;
+                proposal.voters[tokenId] = true;
+            }
+        }
+
+        require(numVotes > 0, "ALREADY_VOTED");
+
+        if(vote == Vote.YAY) {
+            proposal.yayVotes += numVotes;
+        }
+        else{
+            proposal.nayVotes += numVotes;
+        }
+    }
+
+    modifier inactiveProposalOnly(uint256 proposalIndex) {
+        require(proposals[proposalIndex].deadline <= block.timestamp, "DEADLINE_NOT_EXCEEDED");
+
+        require(proposals[proposalIndex].executed == false, "PROPOSAL_ALREADY_EXECUTED");
+        _;
+    }
+
+    function executeProposal(uint256 proposalIndex) external nftHolderOnly inactiveProposalOnly(proposalIndex) {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        //proposal has more Yay votes, purchase NFT from the Fake NFT Marketplace
+        if(proposal.yayVotes > proposal.nayVotes) {
+            uint256 nftPrice = nftMarketplace.getPrice();
+            require(address(this).balance >= nftPrice, "NOT_ENOUGH_FUNDS");
+            nftMarketplace.purchase{value: nftPrice}(proposal.nftTokenId);
+        }
+        proposal.executed = true;
+    }
+
+    function withdrawEther() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    receive() external payable{}
+
+    fallback() external payable {}
+}
